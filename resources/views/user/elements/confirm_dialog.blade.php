b0e4b817c867a12bb4b422f1b04e0e615703709c
Dung Nguyen <dungnt129@gmail.com>
2017-03-25 01:02:28 +0700

    #update spec

--- resources/views/user/elements/confirm_dialog.blade.php
+++ resources/views/user/elements/confirm_dialog.blade.php
@@ -1,10 +1,10 @@
 <div id="delete_confirm" class="modal hide" aria-hidden="false">
 	<div class="modal-header">
 		<button data-dismiss="modal" class="close" type="button">×</button>
-		<h3>Delete Confirm</h3>
+		<h3>Xóa khách hàng</h3>
 	</div>
 	<div class="modal-body">
-		<p>Are you sure you want to delete this customer ?</p>
+		<p>Bạn có chắc là muốn xoá khách hàng này không?</p>
 	</div>
-	<div class="modal-footer"> <a id="delete_confirm_button" class="btn btn-primary" data-href="#">Confirm</a> <a data-dismiss="modal" class="btn" href="#">Cancel</a> </div>
+	<div class="modal-footer"> <a id="delete_confirm_button" class="btn btn-primary" data-href="#">Đồng ý</a> <a data-dismiss="modal" class="btn" href="#">Hủy</a> </div>
 </div>
