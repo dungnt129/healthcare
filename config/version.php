6c9ca4c1bb264d40c9bb5e8655bc3a2687a09f60
Dung Nguyen <dungnt129@gmail.com>
2017-03-29 16:31:53 +0700

    #Fix bug tab name too long

--- config/version.php
+++ config/version.php
@@ -8,7 +8,7 @@ return array(
 	| Prevent cached when making modifications
 	|
 	*/
-	'js' => env('VERSION_JS', '1.0.11'),
+	'js' => env('VERSION_JS', '1.0.12'),
 
 	/*
 	|--------------------------------------------------------------------------
@@ -18,6 +18,6 @@ return array(
 	| Prevent cached when making modifications
 	|
 	*/
-	'css' => env('VERSION_CSS', '1.0.11'),
+	'css' => env('VERSION_CSS', '1.0.12'),
 );
 
